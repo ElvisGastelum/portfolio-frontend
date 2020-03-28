6d80d72dac9da43f1fe37ab485327053bc6804c2
Elvis Gastelum <elvisgastelum@outlook.com>
2020-03-27 21:44:36 -0700

    Create Layout component

--- src/components/Pages/Contact/index.jsx
+++ src/components/Pages/Contact/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import Layout from '../Layout';
+
 import './styles.css';
 
 export default function Contact() {
-    return (
-        <div className="App-contact-default-text">Contact</div>
-    )
+  return (
+    <Layout
+      header="Contact"
+      nav="Navigation"
+      section="Section"
+      article="Article"
+      aside="Aside"
+      footer="Footer"
+    />
+  )
 }
---
+++ src/components/Pages/Layout/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+export default function Layout(props) {
+  return (
+    <div className="container h-100 d-flex flex-column">
+      <header className="mx-auto">{props.header}</header>
+      <nav>{props.nav}</nav>
+      <main className="h-100 d-flex">
+        <div className="w-75">
+          <section className="h-50">{props.section}
+          </section>
+          <article className="h-50">{props.article}
+          </article>
+        </div>
+        <div className="w-25">
+          <aside>{props.aside}</aside>
+        </div>
+      </main>
+      <footer>{props.footer}</footer>
+    </div>
+  )
+}
