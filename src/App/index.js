bb5c94f51cc34e258b169a9ba139c846b7bb3773
Elvis Gastelum <elvisgastelum@outlook.com>
2020-03-31 20:32:49 -0700

    add GalleryImages component using fetch

--- src/App/index.js
+++ src/App/index.js
@@ -3,11 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import { Projects, Contact, NotFound, Home, Layout, Blog } from '../Pages';
 
+import GalleryImages from '../components/GalleryImages';
+
 
 export default function App() {
   return (
     <Layout className="h-100 d-flex flex-column">
       <Switch>
+        <Route exact path='/gallery' component={GalleryImages} />
         <Route exact path='/portfolio/' component={Home} />
         <Route exact path='/portfolio/projects/' component={Projects} />
         <Route exact path='/portfolio/blog/' component={Blog} />
