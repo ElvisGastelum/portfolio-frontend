a65c11c3f4fb4a4aed371f0aac2a147a4e01f330
Elvis Gastelum <elvisgastelum@outlook.com>
2020-04-11 00:44:26 -0700

    add https in request for fake articles

--- src/services/fakeArticles.js
+++ src/services/fakeArticles.js
@@ -1,4 +1,4 @@
-const BASE_URL_ARTICLES_LIST = process.env.REACT_APP_BASE_URL_ARTICLES_LIST || 'http://newsapi.org/v2';
+const BASE_URL_ARTICLES_LIST = process.env.REACT_APP_BASE_URL_ARTICLES_LIST || 'https://newsapi.org/v2';
 
 const getArticles = async ({ country = "mx" }) => {
   const url = `${BASE_URL_ARTICLES_LIST}/top-headlines?country=${country}&apiKey=6000bc1aeed64d458affdc47fb7ce2d5`;
